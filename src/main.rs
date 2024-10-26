fd43a452be4c8e995669732f374076ee27bd89c2
vars1ty <gh@resistant.ch>
2024-10-26 09:34:18 +0200

    misc: Safety improvements and add windows check

--- src/main.rs
+++ src/main.rs
@@ -60,8 +60,8 @@ impl Greenland {
     fn perform_workspace_check(&'static self) {
         let id = Self::execute("hyprctl activeworkspace -j | jq -r .id")
             .expect("[ERROR] Failed obtaining Workspace ID!");
-        let preset = match id.parse::<u8>().unwrap_or_default() {
-            1 | 3 | 4 => "performance",
+        let preset = match id.as_str() {
+            "1" | "3" | "4" => "performance",
             _ => "powersave",
         };
 
@@ -90,23 +90,27 @@ impl Greenland {
 
     /// Checks the value of `self.secs_since_cursor_update`, if it's above *x* then the PC is put
     /// into hibernation.
+    /// ## Times
+    /// **25 minutes** before a warning if there are windows present, otherwise **5 minutes**.
+    /// **30 minutes** before hibernation if there are windows present, otherwise **10 minutes**.
     fn try_hibernate(&self) {
-        match self.secs_since_cursor_update.load(Ordering::Relaxed) {
-            // 25 minutes
-            1500 => {
-                // Not a good workaround, but it'll do for now as sudo breaks regular notify-send.
-                unsafe {
-                    system(
+        let elapsed = self.secs_since_cursor_update.load(Ordering::Relaxed);
+        let warning_secs = if self.has_windows() { 1500 } else { 300 };
+        let hibernate_secs = if self.has_windows() { 1800 } else { 600 };
+
+        if elapsed == warning_secs {
+            // Not a good workaround, but it'll do for now as sudo breaks regular notify-send.
+            unsafe {
+                system(
                     cr#"hyprctl dispatch exec 'notify-send -u critical -a "Greenland" "Putting PC into hibernation in 5 minutes, move your cursor to prevent it!"'"#.as_ptr() as _,
                 )
-                };
-            }
-            // 30 minutes
-            1800 => {
-                self.secs_since_cursor_update.store(1, Ordering::Relaxed);
-                unsafe { system(c"systemctl suspend".as_ptr()) };
-            }
-            _ => {}
+            };
+            return;
+        }
+
+        if elapsed == hibernate_secs {
+            self.secs_since_cursor_update.store(1, Ordering::Relaxed);
+            unsafe { system(c"systemctl suspend".as_ptr()) };
         }
     }
 
@@ -118,6 +122,13 @@ impl Greenland {
         *last_cursor_pos =
             Self::execute("hyprctl cursorpos").expect("[ERROR] Failed getting cursor position!");
         *last_cursor_pos != last_cursor_pos_clone
+    }
+
+    /// Checks if the workspace has any windows present.
+    fn has_windows(&self) -> bool {
+        Self::execute("hyprctl activeworkspace -j | jq -r .windows")
+            .expect("[ERROR] Failed getting workspace windows count!")
+            != "0"
     }
 
     /// Executes a command and returns the output.
