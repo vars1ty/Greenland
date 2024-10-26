eed13960f1700ab43c0a3b8367612717458e8476
vars1ty <gh@resistant.ch>
2024-10-26 09:47:48 +0200

    refactor: Remove need for thread-safe code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
 use gag::Gag;
-use std::{
-    ffi::CString,
-    sync::{
-        atomic::{AtomicU32, Ordering},
-        Mutex, Once,
-    },
-};
+use std::{ffi::CString, sync::Once};
 
 extern "C" {
     /// C `system("echo \"Hello World!\"")` bind.
@@ -29,18 +23,18 @@ fn main() {
 #[derive(Default)]
 pub struct Greenland {
     /// Seconds elapsed since the last cursor position update.
-    secs_since_cursor_update: AtomicU32,
+    secs_since_cursor_update: u32,
 
     /// Last `hyprctl cursorpos` output.
-    last_cursor_pos: Mutex<String>,
+    last_cursor_pos: String,
 }
 
 impl Greenland {
     /// Starts all of the background checks.
-    pub fn start(&'static self) {
+    pub fn start(&mut self) {
         static START: Once = Once::new();
         START.call_once(|| {
-            self.secs_since_cursor_update.store(1, Ordering::Relaxed);
+            self.secs_since_cursor_update = 1;
 
             loop {
                 self.perform_workspace_check();
@@ -52,7 +46,7 @@ impl Greenland {
     }
 
     /// Performs the workspace check, which change the CPU frequency preset based on the ID.
-    fn perform_workspace_check(&'static self) {
+    fn perform_workspace_check(&self) {
         let id = Self::execute("hyprctl activeworkspace -j | jq -r .id")
             .expect("[ERROR] Failed obtaining Workspace ID!");
         let preset = match id.as_str() {
@@ -67,19 +61,13 @@ impl Greenland {
 
     /// Performs the hibernation check, which keeps track of the cursor position through `hyprctl`.
     /// If it hasn't moved after a certain period of time, hibernation is activated.
-    fn perform_hibernation_check(&'static self) {
-        self.secs_since_cursor_update
-            .fetch_add(1, Ordering::Relaxed);
-        let mut last_cursor_pos = self
-            .last_cursor_pos
-            .lock()
-            .expect("[ERROR] Failed accessing self.last_cursor_pos!");
-
-        if !self.has_cursor_moved(&mut last_cursor_pos) {
+    fn perform_hibernation_check(&mut self) {
+        self.secs_since_cursor_update += 1;
+        if !Self::has_cursor_moved(&mut self.last_cursor_pos) {
             self.try_hibernate();
         } else {
             // Reset `secs_since_cursor_update` as the cursor was moved.
-            self.secs_since_cursor_update.store(1, Ordering::Relaxed);
+            self.secs_since_cursor_update = 1;
         }
     }
 
@@ -88,12 +76,11 @@ impl Greenland {
     /// ## Times
     /// **25 minutes** before a warning if there are windows present, otherwise **5 minutes**.
     /// **30 minutes** before hibernation if there are windows present, otherwise **10 minutes**.
-    fn try_hibernate(&self) {
-        let elapsed = self.secs_since_cursor_update.load(Ordering::Relaxed);
+    fn try_hibernate(&mut self) {
         let warning_secs = if self.has_windows() { 1500 } else { 300 };
         let hibernate_secs = if self.has_windows() { 1800 } else { 600 };
 
-        if elapsed == warning_secs {
+        if self.secs_since_cursor_update == warning_secs {
             // Not a good workaround, but it'll do for now as sudo breaks regular notify-send.
             unsafe {
                 system(
@@ -103,8 +90,8 @@ impl Greenland {
             return;
         }
 
-        if elapsed == hibernate_secs {
-            self.secs_since_cursor_update.store(1, Ordering::Relaxed);
+        if self.secs_since_cursor_update == hibernate_secs {
+            self.secs_since_cursor_update = 1;
             unsafe { system(c"systemctl suspend".as_ptr()) };
         }
     }
@@ -112,7 +99,7 @@ impl Greenland {
     /// Takes `self.last_cursor_pos` as mutable String reference, clones it and updates the
     /// mutable references value to the current cursor position.
     /// Then checks if the two values are identical and returns the result.
-    fn has_cursor_moved(&self, last_cursor_pos: &mut String) -> bool {
+    fn has_cursor_moved(last_cursor_pos: &mut String) -> bool {
         let last_cursor_pos_clone = last_cursor_pos.to_owned();
         *last_cursor_pos =
             Self::execute("hyprctl cursorpos").expect("[ERROR] Failed getting cursor position!");
